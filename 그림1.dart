f9a42bc9a366c4aa54157ebaeef5a1261a49efc3
KOYOUNGGIL <koyounggil@naver.com>
2024-08-22 16:36:53 +0900

    Create 그림1.dart

---
+++ 그림1.dart
@@ -0,0 +1,24 @@
+void main() {
+  // variable Case.1 
+  int intTemp = 1 ;
+  print(intTemp);
+  double dblTemp = 2.2 ;
+  print(dblTemp);
+  num numIntTemp = 3;
+  print(numIntTemp);
+  num numdblTemp = 4.4;
+  print(numdblTemp);
+  String strTemp = "Hello!";
+  print(strTemp);
+  
+  // // variable Case.2 
+  var varInt = 1;
+  var varDouble = 2.2;
+  var varString = "Hello!";
+  print("$varInt $varDouble $varString");
+  
+  // Constant Case.3
+  dynamic dynInt = 1;
+  dynamic dynDouble = 2.2;
+  dynamic dynString = "Hello!";
+  print("$dynInt $dynDouble $dynString");
