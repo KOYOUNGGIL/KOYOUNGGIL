7a2409259a8a3815d20e4706dade325afc96c39d
KOYOUNGGIL <koyounggil@naver.com>
2024-08-30 10:25:34 +0900

    Update 리스트활용예제.dart

--- 리스트활용예제.dart
+++ 리스트활용예제.dart
@@ -55,4 +55,29 @@ void main() {
   print(hasApple);
   bool hasMelon = fruits.contains('Melon');
   print(hasMelon);
+  
+  // 리스트 관련 기능설명
+  // length 리스트에 속한 항목의 개수 계산
+  // first  리스트에 속한 첫번째 항목을 확인
+  // last   리스트에 속한 마지막 항목을 확인
+  // indexOf 리스트에 속한 특정 위치의 항목을 확인
+  // insert 리스트의 특정 위치에 새로운 항목을 추가
+  // add    리스트의 끝ㅇ[(전달받은 리스트의) 모든 항목을 추가
+  // sort   리스트의 항목을 순서대로 정렬
+  // clear  리스트의 모든 항목을 삭제
+
+
+  
+  print("[01] fruits is $List");
+  print("[02] Length of fruits is ${fruits.length}");
+  print("[03] First element of fruits is ${fruits.first}");
+  print("[04] Last element of fruits is ${fruits.last}");
+  
+  //아래문장은 type 으로 인한 에러 발생함 원인 파악 필요
+  //print("[05] Index of 3 in fruits is ${fruits.indexOf(3)}");
+  print(fruits[3]);
+  fruits.insert(2, "Melon");
+  print(fruits);
+  print("[06] fruits after insert(2,Melon) is $fruits");
+
 }
